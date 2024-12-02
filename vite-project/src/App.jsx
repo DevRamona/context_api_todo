4fe3c7d8f00f3dab474a5c9b35258478be49ddac
DevRamona <r.ingabire@alustudent.com>
2024-12-02 18:38:48 +0200

    refactor: change the title

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { TodoProvider } from './TodoContext';
 import TodoList from './TodoList';
 
@@ -6,7 +5,7 @@ function App() {
   return (
     <TodoProvider>
       <div className="App">
-        <h1 className="text-3xl font-bold text-center my-8">Todo App</h1>
+        <h1 className="text-3xl font-bold text-center my-8">Todo List App</h1>
         <TodoList />
       </div>
     </TodoProvider>
