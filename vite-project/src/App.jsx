c9effb3ec987d083ffa172545266659c75a07c52
DevRamona <r.ingabire@alustudent.com>
2024-12-02 18:46:23 +0200

    fix:remove the errors

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <TodoProvider>
       <div className="App">
-        <h1 className="text-3xl font-bold text-center my-8">Todo List App</h1>
+        <h1 className="text-3xl font-bold text-green-500 text-center my-8">Todo List App</h1>
         <TodoList />
       </div>
     </TodoProvider>
